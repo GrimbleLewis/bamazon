45012f3dc5d1c0452b1662b34012eb3459739f7e
Grimble <grimblelewisthethird@gmail.com>
2019-04-28 23:15:49 -0400

    fixed the db name spelling and created a products table, need to insert values

--- bamazonDB.sql
+++ bamazonDB.sql
@@ -1,4 +1,43 @@
-DROP DATABASE IF EXISTS bamanzonDB;
-CREATE database bamanzonDB;
+DROP DATABASE IF EXISTS bamazonDB;
+CREATE database bamazonDB;
 
-USE bamanzonDB;
+USE bamazonDB;
+
+CREATE TABLE products (
+    item_id INT NOT NULL auto_increment,
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(100) NULL,
+    price INT NOT NULL,
+    stock_quantity INT NOT NULL
+    PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ();
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ();
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ();
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ();
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ();
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ();
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ();
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ();
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ();
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ();
