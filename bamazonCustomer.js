b62f0483d5c678d0c74a45ef2096e4bdc7ac7996
GrimbleLewis <grimblelewisthethird@gmail.com>
2019-05-01 16:55:17 -0400

    have the items display neatly and prompt the user with questions about which item and how many to buy. Working on checking if the amount the user wants is over the quantity of the product in the database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,26 +2,85 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    port: 3306,
-  
-    user: "root",
-  
-    password: "W00tl00t@",
-    database: "bamazonDB"
-  });
-  
-  connection.connect(function(err) {
+  host: "localhost",
+
+  port: 3306,
+
+  user: "root",
+
+  password: "W00tl00t@",
+  database: "bamazonDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  showProducts();
+});
+
+function showProducts() {
+  var sql = "SELECT item_id,product_name,price from products";
+  connection.query(sql, function(err, res) {
     if (err) throw err;
-    showProducts();
+    for (var i = 0; i < res.length; i++) {
+      console.log(
+        "Item ID: " +
+          res[i].item_id +
+          " || Product: " +
+          res[i].product_name +
+          " || Price: " +
+          res[i].price
+      );
+    }
+    purchaseProduct();
   });
+}
 
-  function showProducts () {
-    var sql = "SELECT * from products"
-
-    connection.query(sql, function (err, result){
+function purchaseProduct() {
+  inquirer
+    .prompt([
+      {
+        name: "item",
+        type: "input",
+        message:
+          "What is the item ID number of the product you would like to buy?",
+          validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            console.log("  Please provide a number\n");
+            return false;
+          }
+      },
+      {
+        name: "amount",
+        type: "input",
+        message: "How many units of this product would you like to buy?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          } 
+          console.log("  Please provide a number\n");
+          return false;
+        }
+      }
+    ])
+    .then(function(answer) {
+      connection.query("SELECT item_id,stock_quantity from products",
+      function(err, res) {
         if (err) throw err;
-        console.log(result);
-    })
-  }
+        var chosenItem;
+        for (var i = 0; i < res.length; i++) {
+          if (res[i].item_id === answer.item) {
+            chosenItem = res[i];
+          }
+        }
+
+      if (chosenItem < parseInt(answer.amount)){
+        console.log("Toomuch")
+      } else {
+        console.log("other")
+      }
+      
+    });
+});
+}
